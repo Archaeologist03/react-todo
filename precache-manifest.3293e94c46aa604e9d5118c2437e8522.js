812c2080694f87cdc383ca4672cd61c947e9de63
Archaeologist03 <omniiknight@hotmail.com>
2019-05-01 22:27:05 +0200

    Updates

---
+++ precache-manifest.3293e94c46aa604e9d5118c2437e8522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d87e21c889be9ca7d95192d37886d01",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "9f7cca4d0872b5e05c1f",
+    "url": "/react-todo/static/css/main.67008072.chunk.css"
+  },
+  {
+    "revision": "df91306cce3b5db64504",
+    "url": "/react-todo/static/js/2.ef1c44b8.chunk.js"
+  },
+  {
+    "revision": "9f7cca4d0872b5e05c1f",
+    "url": "/react-todo/static/js/main.e97efb5d.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "7071c46a6a1ecceb8d83919dc6f132be",
+    "url": "/react-todo/static/media/todo-orange.7071c46a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/react-todo/precache-manifest.8fe6f432beda04ada5017993c783545f.js"
+  "/react-todo/precache-manifest.3293e94c46aa604e9d5118c2437e8522.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-todo/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
